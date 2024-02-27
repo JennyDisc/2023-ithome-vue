0dfe53aeeef1baf67c182feac4e44981f555254d
JennyDisc <touki741@hotmail.com>
2024-02-27 18:59:15 +0800

    deploy

---
+++ assets/LoginStore-98680ae9.js
@@ -0,0 +1,1 @@
+import{k as t}from"./index-48e79dc0.js";const i=t({id:"myStore",state:()=>({name:"",email:"",id:0}),actions:{updateName(e){this.name=e},updateEmail(e){this.email=e},updateId(e){this.id=e}}});export{i as u};
